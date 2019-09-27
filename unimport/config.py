ec1df38ce13dfff7016af12184de58a1bf208644
hakan çelik <hakancelik96@outlook.com>
2019-09-27 23:13:49 +0300

    config bug fix, folders and files confs section add

--- unimport/config.py
+++ unimport/config.py
@@ -1,11 +1,27 @@
 import configparser
 import os
 import pathlib
+import re
+import sys
 
 CONFIG = configparser.ConfigParser(allow_no_value=True)
 BUILTIN_CONFIG = pathlib.Path(__file__).parent / ".unimport.cfg"
-CONFIG.read(BUILTIN_CONFIG)
-
-EXTRA_CONFIG = str(pathlib.Path().cwd()) + os.sep + ".unimport.cfg"
+try:
+    directory = os.sep + sys.argv[1] + os.sep
+except IndexError:
+    directory = os.sep
+EXTRA_CONFIG = str(pathlib.Path().cwd()) + directory + ".unimport.cfg"
 if os.path.exists(EXTRA_CONFIG):
     CONFIG.read(EXTRA_CONFIG)
+CONFIG.read(BUILTIN_CONFIG)
+
+IGNORE_FOLDERS = re.compile("|".join(set([i for i in CONFIG["folders"]])))
+IGNORE_FILES = re.compile("|".join(set([i for i in CONFIG["files"]])))
+
+
+def is_ignore_folder(path):
+    return IGNORE_FOLDERS.match(path) != None
+
+
+def is_ignore_files(path):
+    return IGNORE_FILES.match(path) != None
--- unimport/console.py
+++ unimport/console.py
@@ -2,7 +2,8 @@ import os
 import sys
 import tokenize
 
-from unimport import files, unused
+from unimport.files import get_files
+from unimport.unused import get_unused
 
 
 def get_unused_imports(file_path):
@@ -11,8 +12,7 @@ def get_unused_imports(file_path):
     except OSError:
         pass
     else:
-        unused_imports = unused.get_unused(source=source)
-        for imports in unused_imports:
+        for imports in get_unused(source=source):
             imports.update(path=file_path.replace(os.getcwd(), ""))
             yield imports
 
@@ -24,7 +24,7 @@ def console_scripts():
         source_file_or_directory = "."
     if os.path.isdir(source_file_or_directory):
         # folder
-        for file in files.get_files(source_file_or_directory):
+        for file in get_files(source_file_or_directory):
             for un_used in get_unused_imports(file):
                 print(un_used)
     else:
@@ -33,3 +33,7 @@ def console_scripts():
         if file_path.endswith(".py"):
             for un_used in get_unused_imports(file_path):
                 print(un_used)
+
+
+if __name__ == "__main__":
+    console_scripts()
