837af61fab771b4a09893a7854df42452de7aff9
Gökmen Görgen <gkmngrgn@gmail.com>
2019-12-05 22:37:19 +0100

    argparse support.

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,18 +6,15 @@ from unimport.config import DEFAULT_IGNORED_FILES, DEFAULT_IGNORED_FOLDERS, Conf
 TEST_DIR = pathlib.Path(__file__).parent
 TEST_DATA = [
     {
-        "config_dir": TEST_DIR / "samples" / "unimport_cfg",
-        "config_path": TEST_DIR / "samples" / "unimport_cfg" / ".unimport.cfg",
+        "config_file": TEST_DIR / "samples" / ".unimport.cfg",
         "section": None,
     },
     {
-        "config_dir": TEST_DIR / "samples" / "pyproject_toml",
-        "config_path": TEST_DIR / "samples" / "pyproject_toml" / "pyproject.toml",
+        "config_file": TEST_DIR / "samples" / "pyproject.toml",
         "section": "tool.unimport",
     },
     {
-        "config_dir": TEST_DIR / "samples" / "setup_cfg",
-        "config_path": TEST_DIR / "samples" / "setup_cfg" / "setup.cfg",
+        "config_file": TEST_DIR / "samples" / "setup.cfg",
         "section": "unimport",
     },
 ]
@@ -26,9 +23,9 @@ TEST_DATA = [
 class ConfigTest(TestCase):
     def test_find_config(self):
         for datum in TEST_DATA:
-            config = Config(config_dir=datum["config_dir"])
+            config = Config(config_file=datum["config_file"])
             actual_path, actual_section = config.find_config()
-            self.assertEqual(actual_path, datum["config_path"])
+            self.assertEqual(actual_path, datum["config_file"])
             self.assertEqual(actual_section, datum["section"])
 
     def test_parse_config(self):
@@ -40,6 +37,6 @@ class ConfigTest(TestCase):
 
         for datum in TEST_DATA:
             print(datum["section"])
-            config = Config(config_dir=datum["config_dir"])
+            config = Config(config_file=datum["config_file"])
             self.assertSetEqual(config.ignored_folders, expected_folders)
             self.assertSetEqual(config.ignored_files, expected_files)
