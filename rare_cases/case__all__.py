cc70eb9f02ee9697eb0023dd34a5f981a58458fd
Hakan Celik <hakancelikdev@gmail.com>
2020-01-07 20:14:34 +0300

    try_except case add

--- rare_cases/case__all__.py
+++ rare_cases/case__all__.py
@@ -1,5 +1,12 @@
 # This is not unused import, but it is unused import according to unimport.
 
+# CASE 1
 from codeop import compile_command
 
 __all__ = ["compile_command"]
+
+# CASE 2; NOTE Its has methods need to be checked
+# then they need to be compared to the state of use and then it has to be printed.
+from io import __all__ 
+# or
+from io import * 
