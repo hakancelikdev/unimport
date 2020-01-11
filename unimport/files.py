f00a682e86bf9f571274cf4c3a2fd78fc80fb161
Hakan Celik <hakancelikdev@gmail.com>
2020-01-11 13:44:51 +0300

    #8 & glob configuration

--- unimport/files.py
+++ unimport/files.py
@@ -7,29 +7,17 @@ from unimport.unused import filter_unused_imports
 
 
 def get_files(src, config):
-    ignored_folders = re.compile("|".join(set(config.ignored_folders)))
-    ignored_files = re.compile("|".join(set(config.ignored_files)))
+    p = pathlib.Path(src)
 
-    def _is_ignored_folder(path):
-        return ignored_folders.match(path) is not None
+    def _is_ignored(path):
+        for ignore_rule in config.ignore:
+            for i in p.glob(ignore_rule):
+                if str(path).startswith(str(i)):
+                    return True
 
-    def _is_ignored_file(path):
-        return (
-            not path.endswith(".py") or ignored_files.match(path) is not None
-        )
-
-    if src.is_dir():
-        for root, dirs, files in os.walk(src):
-            if _is_ignored_folder(root):
-                continue
-
-            for name in files:
-                file_path = os.path.join(root, name)
-                if not _is_ignored_file(file_path):
-                    yield file_path
-
-    elif not _is_ignored_file(src.name):
-        yield str(src)
+    for file in p.glob("**/*.py"):
+        if not _is_ignored(file):
+            yield file
 
 
 def overwrite(file_path, unused_imports):
