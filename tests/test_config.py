abd065c20941ed90e8c0013a280703451afae9e5
Hakan Celik <hakancelikdev@gmail.com>
2020-06-04 22:07:10 +0300

    config test rewrite using session

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,8 +2,7 @@ from pathlib import Path
 from unittest import TestCase
 
 from unimport import config as CONFIG
-
-Config = CONFIG.Config
+from unimport.session import Session
 
 TEST_DIR = Path(__file__).parent
 
@@ -22,45 +21,52 @@ class TestConfig(TestCase):
     def setUp(self):
         self.include = "test|test2|tests.py"
         self.exclude = "__init__.py|tests/"
+        self.config_toml = Session(
+            config_file=CONFIG_TOML["config_file"]
+        ).config
+        self.config_cfg = Session(config_file=CONFIG_CFG["config_file"]).config
 
     def test_toml_find_config(self):
-        config = Config(config_file=CONFIG_TOML["config_file"])
-        actual_path, actual_section = config.find_config()
+        actual_path, actual_section = self.config_toml.find_config()
         self.assertEqual(actual_path, CONFIG_TOML["config_file"])
         self.assertEqual(actual_section, CONFIG_TOML["section"])
 
     def test_toml_attr(self):
-        config = Config(config_file=CONFIG_TOML["config_file"])
-        self.assertEqual(self.include, config.include)
-        self.assertEqual(self.exclude, config.exclude)
+        self.assertEqual(self.include, self.config_toml.include)
+        self.assertEqual(self.exclude, self.config_toml.exclude)
 
     def test_toml_is_available_to_parse(self):
         setattr(CONFIG, "HAS_TOML", True)
-        config = Config(config_file=CONFIG_TOML["config_file"])
-        self.assertTrue(config.is_available_to_parse(config.config_file))
+        self.assertTrue(
+            self.config_toml.is_available_to_parse(
+                self.config_toml.config_file
+            )
+        )
 
     def test_toml_but_has_toml_false(self):
         setattr(CONFIG, "HAS_TOML", False)
-        config = Config(config_file=CONFIG_TOML["config_file"])
-        self.assertFalse(config.is_available_to_parse(config.config_file))
+        self.assertFalse(
+            self.config_toml.is_available_to_parse(
+                self.config_toml.config_file
+            )
+        )
 
     def test_cfg_find_config(self):
-        config = Config(config_file=CONFIG_CFG["config_file"])
-        actual_path, actual_section = config.find_config()
+        actual_path, actual_section = self.config_cfg.find_config()
         self.assertEqual(actual_path, CONFIG_CFG["config_file"])
         self.assertEqual(actual_section, CONFIG_CFG["section"])
 
     def test_cfg_attr(self):
-        config = Config(config_file=CONFIG_CFG["config_file"])
-        self.assertEqual(self.include, config.include)
-        self.assertEqual(self.exclude, config.exclude)
+        self.assertEqual(self.include, self.config_cfg.include)
+        self.assertEqual(self.exclude, self.config_cfg.exclude)
 
     def test_cfg_is_available_to_parse(self):
-        config = Config(config_file=CONFIG_CFG["config_file"])
-        self.assertTrue(config.is_available_to_parse(config.config_file))
+        self.assertTrue(
+            self.config_cfg.is_available_to_parse(self.config_cfg.config_file)
+        )
 
     def test_config_file_none_find_config(self):
-        config = Config(config_file=None)
-        actual_path, actual_section = config.find_config()
+        none_config = Session(config_file=None).config
+        actual_path, actual_section = none_config.find_config()
         self.assertIsNone(actual_path)
         self.assertIsNone(actual_section)
