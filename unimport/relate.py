353cd6cc2e88c19716306c802c4ed1c3e8188138
Hakan Çelik <hakancelik96@outlook.com>
2020-07-13 01:54:31 +0300

    relate.py

--- unimport/relate.py
+++ unimport/relate.py
@@ -1,23 +1,24 @@
 # https://tree.science/using-ancestral-chains-in-ast.html
 
 import ast
+from typing import Iterator
 
 
-def relate(tree):
-    tree.parent = None
+def relate(tree: ast.Module) -> None:
+    tree.parent = None # type: ignore
     for parent in ast.walk(tree):
         for child in ast.iter_child_nodes(parent):
-            child.parent = parent
+            child.parent = parent  # type: ignore
 
 
-def get_parents(node):
+def get_parents(node: ast.AST) -> Iterator[ast.AST]:
     parent = node
     while parent:
-        parent = parent.parent
+        parent = parent.parent   # type: ignore
         yield parent
 
 
-def first_occurrence(node, *ancestors):
+def first_occurrence(node: ast.AST, *ancestors):
     for parent in get_parents(node):
         if type(parent) in ancestors:
             return parent
