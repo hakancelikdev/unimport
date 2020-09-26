afee861cb6bbb02ff2114f2a5385d5641c1f8494
Hadi Zaki Alqattan <alqattanhadizaki@gmail.com>
2020-09-26 17:17:22 +0300

    Support: Windows OS. (#116)

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,8 +1,32 @@
+import os
 import tempfile
 import unittest
+from contextlib import contextmanager
 from pathlib import Path
+from typing import Iterator
 
 from unimport.session import Session
+
+
+@contextmanager
+def reopenable_temp_file(content: str) -> Iterator[Path]:
+    """Reopenable tempfile to support writing/reading to/from the opened
+    tempfile (requiered for Windows OS).
+
+    For more information: https://bit.ly/3cr0Qkl
+
+    :param content: string content to write.
+    :yields: tempfile path.
+    """
+    try:
+        with tempfile.NamedTemporaryFile(
+            mode="w", suffix=".py", encoding="utf-8", delete=False
+        ) as tmp:
+            tmp_path = Path(tmp.name)
+            tmp.write(content)
+        yield tmp_path
+    finally:
+        os.unlink(tmp_path)
 
 
 class TestSession(unittest.TestCase):
@@ -18,12 +42,8 @@ class TestSession(unittest.TestCase):
                 self.assertTrue(str(p).endswith(".py"))
 
     def temp_refactor(self, source: str, expected: str, apply: bool = False):
-        with tempfile.NamedTemporaryFile(mode="w", suffix=".py") as tmp:
-            tmp.write(source)
-            tmp.seek(0)
-            result, _ = self.session.refactor_file(
-                path=Path(tmp.name), apply=apply
-            )
+        with reopenable_temp_file(source) as tmp_path:
+            result, _ = self.session.refactor_file(path=tmp_path, apply=apply)
             self.assertEqual(result, expected)
 
     def test_bad_encoding(self):
@@ -43,12 +63,10 @@ class TestSession(unittest.TestCase):
         self.assertEqual(diff, self.session.diff("import os"))
 
     def test_diff_file(self):
-        with tempfile.NamedTemporaryFile(mode="w", suffix=".py") as tmp:
-            tmp.write("import os")
-            tmp.seek(0)
-            diff_file = self.session.diff_file(path=Path(tmp.name))
+        with reopenable_temp_file("import os") as tmp_path:
+            diff_file = self.session.diff_file(path=tmp_path)
             diff = (
-                f"--- {tmp.name}\n",
+                f"--- {tmp_path.as_posix()}\n",
                 "+++ \n",
                 "@@ -1 +0,0 @@\n",
                 "-import os",
@@ -57,9 +75,5 @@ class TestSession(unittest.TestCase):
 
     def test_read(self):
         source = "b�se"
-        with tempfile.NamedTemporaryFile(mode="w", suffix=".py") as tmp:
-            tmp.write(source)
-            tmp.seek(0)
-            self.assertEqual(
-                (source, "utf-8"), self.session.read(Path(tmp.name))
-            )
+        with reopenable_temp_file(source) as tmp_path:
+            self.assertEqual((source, "utf-8"), self.session.read(tmp_path))
