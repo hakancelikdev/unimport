4b1b5ee19341fc58fc607b11736dffa751927339
Hakan Çelik <hakancelik96@outlook.com>
2020-09-30 23:45:25 +0300

    fix typing

--- unimport/constants.py
+++ unimport/constants.py
@@ -1,5 +1,4 @@
 import sys
-import typing
 
 DESCRIPTION = (
     "A linter, formatter for finding and removing unused import statements."
@@ -8,32 +7,52 @@ VERSION = "0.3.0"
 
 PY38_PLUS = sys.version_info >= (3, 8)
 PY37_PLUS = sys.version_info >= (3, 7)
-PY36_PLUS = sys.version_info >= (3, 6)
 
-PY36 = PY36_PLUS and not PY37_PLUS
-
-if PY37_PLUS:
-    _TypeAncestors = (typing._GenericAlias, typing._SpecialForm)  # type: ignore
-elif PY36_PLUS:
-    _TypeAncestors = (typing.GenericMeta,)  # type: ignore
-
-_typing_variables = [
-    type_name for type_name in dir(typing) if not type_name.startswith("_")
-]
 SUBSCRIPT_TYPE_VARIABLE = [
-    type_name
-    for type_name in _typing_variables
-    if isinstance(getattr(typing, type_name), _TypeAncestors)
+    "AbstractSet",
+    "AsyncContextManager",
+    "AsyncGenerator",
+    "AsyncIterable",
+    "AsyncIterator",
+    "Awaitable",
+    "Callable",
+    "ChainMap",
+    "ClassVar",
+    "Collection",
+    "Container",
+    "ContextManager",
+    "Coroutine",
+    "Counter",
+    "DefaultDict",
+    "Deque",
+    "Dict",
+    "FrozenSet",
+    "Generator",
+    "IO",
+    "ItemsView",
+    "Iterable",
+    "Iterator",
+    "KeysView",
+    "List",
+    "Mapping",
+    "MappingView",
+    "Match",
+    "MutableMapping",
+    "MutableSequence",
+    "MutableSet",
+    "Optional",
+    "Pattern",
+    "Reversible",
+    "Sequence",
+    "Set",
+    "SupportsRound",
+    "Tuple",
+    "Type",
+    "Union",
+    "ValuesView",
 ]
 
-if PY36:
-    SUBSCRIPT_TYPE_VARIABLE.extend(
-        [
-            "Callable",
-            "ClassVar",
-            "Optional",
-            "Tuple",
-            "Type",
-            "Union",
-        ]
-    )
+if PY38_PLUS:
+    SUBSCRIPT_TYPE_VARIABLE.append("Literal")
+elif PY37_PLUS:
+    SUBSCRIPT_TYPE_VARIABLE.append("OrderedDict")
