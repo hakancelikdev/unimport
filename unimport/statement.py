16cccc53118a768e6e556327d012ca7398d23930
Hakan Çelik <hakancelik96@outlook.com>
2020-10-10 20:26:51 +0300

    Update unimport/statement.py

--- unimport/statement.py
+++ unimport/statement.py
@@ -6,7 +6,7 @@ class Name(NamedTuple):
     lineno: int
     name: str
 
-    def match(self, imp: Union["Import", "ImportFrom"]) -> bool:
+    def match(self, imp: Union[Import, ImportFrom]) -> bool:
         return (
             imp.lineno < self.lineno
             and ".".join(self.name.split(".")[: len(imp)]) == imp.name
