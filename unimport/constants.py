d023592cc46a860c71e80d8a87e36e0089303572
Hadi Alqattan <alqattanhadizaki@gmail.com>
2020-10-10 23:27:00 +0300

    Fix: performance issue related to a list usage. (#141)

--- unimport/constants.py
+++ unimport/constants.py
@@ -6,56 +6,56 @@ DESCRIPTION = (
 VERSION = "0.3.0"
 
 PY38_PLUS = sys.version_info >= (3, 8)
-PY37_PLUS = sys.version_info >= (3, 7)
 
-SUBSCRIPT_TYPE_VARIABLE = [
-    "AbstractSet",
-    "AsyncContextManager",
-    "AsyncGenerator",
-    "AsyncIterable",
-    "AsyncIterator",
-    "Awaitable",
-    "Callable",
-    "ChainMap",
-    "ClassVar",
-    "Collection",
-    "Container",
-    "ContextManager",
-    "Coroutine",
-    "Counter",
-    "DefaultDict",
-    "Deque",
-    "Dict",
-    "FrozenSet",
-    "Generator",
-    "IO",
-    "ItemsView",
-    "Iterable",
-    "Iterator",
-    "KeysView",
-    "List",
-    "Mapping",
-    "MappingView",
-    "Match",
-    "MutableMapping",
-    "MutableSequence",
-    "MutableSet",
-    "Optional",
-    "Pattern",
-    "Reversible",
-    "Sequence",
-    "Set",
-    "SupportsRound",
-    "Tuple",
-    "Type",
-    "Union",
-    "ValuesView",
-]
-
-if PY38_PLUS:
-    SUBSCRIPT_TYPE_VARIABLE.extend(["Literal", "OrderedDict"])
-elif PY37_PLUS:
-    SUBSCRIPT_TYPE_VARIABLE.append("Literal")
+SUBSCRIPT_TYPE_VARIABLE = frozenset(
+    {
+        "AbstractSet",
+        "AsyncContextManager",
+        "AsyncGenerator",
+        "AsyncIterable",
+        "AsyncIterator",
+        "Awaitable",
+        "Callable",
+        "ChainMap",
+        "ClassVar",
+        "Collection",
+        "Container",
+        "ContextManager",
+        "Coroutine",
+        "Counter",
+        "DefaultDict",
+        "Deque",
+        "Dict",
+        "FrozenSet",
+        "Generator",
+        "IO",
+        "ItemsView",
+        "Iterable",
+        "Iterator",
+        "KeysView",
+        "List",
+        "Mapping",
+        "MappingView",
+        "Match",
+        "MutableMapping",
+        "MutableSequence",
+        "MutableSet",
+        "Optional",
+        "Pattern",
+        "Reversible",
+        "Sequence",
+        "Set",
+        "SupportsRound",
+        "Tuple",
+        "Type",
+        "Union",
+        "ValuesView",
+        # Python >= 3.7.
+        "Literal",
+        # Python >= 3.8.
+        "OrderedDict",
+    }
+)
 
 INITIAL_IMPORTS = frozenset(
     # https://docs.python.org/3/library/sys.html#sys.modules
