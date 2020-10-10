3614557d70789e0b189e603bf67085d73aaeab51
Hakan Çelik <hakancelik96@outlook.com>
2020-10-10 20:32:33 +0300

    Update statement.py

--- unimport/statement.py
+++ unimport/statement.py
@@ -2,23 +2,12 @@ import operator
 from typing import List, NamedTuple, Union
 
 
-class Name(NamedTuple):
-    lineno: int
-    name: str
-
-    def match(self, imp: Union[Import, ImportFrom]) -> bool:
-        return (
-            imp.lineno < self.lineno
-            and ".".join(self.name.split(".")[: len(imp)]) == imp.name
-        )
-
-
 class Import(NamedTuple):
     lineno: int
     column: int
     name: str
 
-    def __len__(self) -> int:
+    def __len__(self):
         return operator.length_hint(self.name.split("."))
 
 
@@ -29,5 +18,15 @@ class ImportFrom(NamedTuple):
     star: bool
     suggestions: List[str]
 
-    def __len__(self) -> int:
+    def __len__(self):
         return operator.length_hint(self.name.split("."))
+
+
+class Name(NamedTuple):
+    lineno: int
+    name: str
+
+    def match(self, imp: Union[Import, ImportFrom]):
+        if imp.lineno < self.lineno:
+            return ".".join(self.name.split(".")[: len(imp)]) == imp.name
+        return False
