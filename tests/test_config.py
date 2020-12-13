55e010a865fe74c3b241661ffb9f6b4fa7e50c74
Hakan Çelik <hakancelik96@outlook.com>
2020-12-14 00:17:34 +0300

    no unimport section test add

--- tests/test_config.py
+++ tests/test_config.py
@@ -9,6 +9,9 @@ TEST_DIR = Path(__file__).parent / "configs"
 pyproject = TEST_DIR / "pyproject.toml"
 setup_cfg = TEST_DIR / "setup.cfg"
 
+no_unimport_pyproject = TEST_DIR / "no_unimport" / "pyproject.toml"
+no_unimport_setup_cfg = TEST_DIR / "no_unimport" / "setup.cfg"
+
 
 class TestConfig(TestCase):
     include = "test|test2|tests.py"
@@ -109,3 +112,48 @@ class TestTomlCommand(TestCase):
         self.assertFalse(self.config.merge(diff=True).check)
         self.assertFalse(self.config.merge(remove=True).check)
         self.assertFalse(self.config.merge(permission=True).check)
+
+
+class TestNoUnimportSectionTest(TestCase):
+    def setUp(self):
+        self.default_config = DefaultConfig()
+
+    def test_toml_parse(self):
+        config = Config(config_file=no_unimport_pyproject).parse()
+        self.assertEqual(self.default_config.include, config.include)
+        self.assertEqual(self.default_config.exclude, config.exclude)
+        self.assertEqual(self.default_config.sources, config.sources)
+        self.assertFalse(config.gitignore)
+        self.assertFalse(config.requirements)
+        self.assertFalse(config.remove)
+        self.assertFalse(config.diff)
+
+    def test_cfg_parse(self):
+        config = Config(config_file=no_unimport_setup_cfg).parse()
+        self.assertEqual(self.default_config.include, config.include)
+        self.assertEqual(self.default_config.exclude, config.exclude)
+        self.assertEqual(self.default_config.sources, config.sources)
+        self.assertFalse(config.gitignore)
+        self.assertFalse(config.requirements)
+        self.assertFalse(config.remove)
+        self.assertFalse(config.diff)
+
+    def test_cfg_merge(self):
+        config = Config(config_file=no_unimport_setup_cfg).parse()
+        console_configuration = {
+            "include": "tests|env",
+            "remove": True,
+            "diff": False,
+            "include_star_import": True,
+        }
+        config = config.merge(**console_configuration)
+        self.assertEqual("tests|env", config.include)
+        self.assertEqual(self.default_config.exclude, config.exclude)
+        self.assertEqual(self.default_config.sources, config.sources)
+
+        self.assertTrue(config.remove)
+        self.assertTrue(config.include_star_import)
+
+        self.assertFalse(config.gitignore)
+        self.assertFalse(config.requirements)
+        self.assertFalse(config.diff)
