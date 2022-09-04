cd0fd29ed0f2a478bb10ebc40218f6f4b2ec5bdb
Hakan Celik <hakancelikdev@gmail.com>
2022-09-04 05:36:38 +0300

    Update Docs

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,4 @@
+from unimport import constants as C
 import os
 import textwrap
 from pathlib import Path
@@ -26,6 +27,7 @@ def test_list_paths(path, count):
     assert len(list(utils.list_paths(path))) == count
 
 
+@pytest.mark.skipif(not C.PY37_PLUS, reason="0.10.0 and above are only supported for Python 3.7 and above.")
 def test_gitignore_no_error_raise():
     gitignore = textwrap.dedent(
         """\
@@ -40,9 +42,7 @@ def test_gitignore_no_error_raise():
         gitignore_patterns = utils.get_exclude_list_from_gitignore(
             gitignore_path
         )
-        assert list(
-            utils.list_paths(Path("."), gitignore_patterns=gitignore_patterns)
-        ) == [Path("setup.py")]
+        assert list(utils.list_paths(Path("."), gitignore_patterns=gitignore_patterns)) == [Path("setup.py")]
 
 
 def test_bad_encoding():
